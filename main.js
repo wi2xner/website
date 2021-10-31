f517d05d759be57ef5a5aabe351fbbf38452019d
wi2xner <kulikov.winner@gmail.com>
2021-10-31 13:22:38 +0700

    1

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-// ...
-// import (injected) from "../componnents/wallet/connectors"
-//  export default function Home() {
-//  const {active, account, library, connector, activate, deactivate} = useWeb3React()
 
-// async function connect () {
-// 	try {
-// 		await activate(injected)
-// 	} catch(ex){
-// 		console.log (ex)
-// 	}
-
-// } return (
-//  <div>
-//  <button onClick = {connect} class ="CONNECT">Connect</button>
-//  {active ? <span>Connected with <b>{account}</b></span> : <span>Not Connected</span>}
-//  </div>
-//  )}
-
-//  console.log(account);
-//initialize variables and ec functoins
 
 
 hideElement = (element) => element.style.display = "none";
